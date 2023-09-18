4ee06e177f76387ff2e5989541d84ac8365ccbb0
eddiemuhoro <eddiemuhoro@gmail.com>
2023-09-18 20:50:21 +0300

    (fix) fixed background colors

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -18,11 +18,11 @@ const Products =  async () => {
     const products: Product[] = await res.json()   
   
   return (
-    <div className="container mx-auto py-8 px-8">
+    <div className="container mx-auto py-8 px-8 bg-slate-100">
       <h1 className="text-3xl font-bold mb-4">Products for Sale</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         {products.map((product) => (
-          <Link href={`/products/${product.id}`} key={product.id} className="block rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-transform transform hover:scale-105 hover:shadow-lg">
+          <Link href={`/products/${product.id}`} key={product.id} className="block rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-transform transform hover:scale-105 hover:shadow-lg bg-white">
             
               <div className="relative h-40">
                 <Image
